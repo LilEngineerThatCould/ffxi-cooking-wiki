d751c434f13187d372359ef3c8b66fe3397d1340
Lynnette Munez <lynnette18M@gmail.com>
2024-05-16 19:18:50 -0400

    added base components and created base for recipe component

---
+++ src/components/Layout/Header/Header.js
@@ -0,0 +1,13 @@
+import './styles.css';
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <h1 className='headerTitle'>FFXI Cooking Wiki</h1>
+            </header>
+        </>
+    );
+};
+
+export default Header;
---
+++ src/components/RecipeDetails/RecipeDetails.js
@@ -0,0 +1,58 @@
+const RecipeDetails = () => {
+    return (
+        <>
+            <h2>Boiled Crayfish</h2>
+            <p>A crayfish boiled in a salty broth.</p>
+            <section>
+                <p>Crystal</p>
+                <p>Fire Crystal</p>
+            </section>
+            <section>
+                <h3>Ingredients</h3>
+                <ul>
+                    <li>1 Crayfish</li>
+                    <li>1 Rock Salt</li>
+                    <li>1 Distiled Water</li>
+                </ul>
+            </section>
+            <section>
+                <p>Yeilds:</p>
+                <p>1 Boiled Crayfish</p>
+            </section>
+            <section>
+                <h3>Nutrition Facts</h3>
+                <p>defense</p>
+            </section>
+            <section>
+                <p>Cooking Level: 9</p>
+            </section>
+            <section>
+                <p>Stackable: 12</p>
+            </section>
+            <section>
+                <h3>Food Effects:</h3>
+                <p>Duration: 30 mins</p>
+                <p>All Races</p>
+                <p>Defense +30% (Max. 25 @ 84 Base Defense)</p>
+            </section>
+            <section>
+                <h3>Other Uses</h3>
+                <p>Guild Points Value: 150 / 5,760 (38.4 items)</p>
+                <p>Used in Mission: Materiel Storm II (W)</p>
+                <p>Resale Price: 96~102 gil</p>
+            </section>
+            <section>
+                <h3>How to Obtain</h3>
+                <h4>Auction House</h4>
+                <p>
+                    Food - Meals - Seafood Ffxiah-small Can be obtained as a random reward from the
+                    Gobbie Mystery Box Special Dial and similar sources.
+                </p>
+                <h4>Merchants</h4>
+                <p>Croumangue - Port San d'Oria (G-7)</p>
+            </section>
+        </>
+    );
+};
+
+export default RecipeDetails;
